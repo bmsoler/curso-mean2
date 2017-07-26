9ef7f07a19ade304d3ac4df6c1da2aedf51e3973
Blas Mateo Soler <blas@MacBook-Pro-de-Blas.local>
2017-07-26 23:57:59 +0200

    cliente

--- cliente/src/app/app.component.ts
+++ cliente/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public title = 'MUSIFY';
+  public user: User;
+  public identity = true; 
+  public token;
+
+  constructor(){
+  	this.user = new User('','','','','','ROLE_USER','');
+  }
 }
